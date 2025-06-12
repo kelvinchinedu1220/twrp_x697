76dcb008112dc0f63dd054314a3b40c45e73eab6
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-06-12 19:52:43 +0100

    file upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_Infinix-X697.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_Infinix-X697-user \
+    omni_Infinix-X697-userdebug \
+    omni_Infinix-X697-eng
