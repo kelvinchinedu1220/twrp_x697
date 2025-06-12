89ed92c22c004094b898a19655e18e5855ee6aea
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-06-12 23:19:52 +0100

    Update and rename omni_Infinix-X697.mk to twrp_X697.mk

---
+++ twrp_X697.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit from X697 device
+$(call inherit-product, device/infinix/X697/device.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := X697
+PRODUCT_NAME := twrp_X697
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix X697
+PRODUCT_MANUFACTURER := infinix
